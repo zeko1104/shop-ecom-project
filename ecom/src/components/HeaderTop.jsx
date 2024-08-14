6dc48d2533549e15c50729dbfde830e4e294975e
Zeko706 <131792180+Zeko706@users.noreply.github.com>
2024-08-14 21:06:44 +0400

    second commint

--- ecom/src/components/HeaderTop.jsx
+++ ecom/src/components/HeaderTop.jsx
@@ -9,24 +9,34 @@ import { FaTiktok } from "react-icons/fa";
 
 export default function HeaderTop() {
   return (
-    <div className="bg-black w-full h-10">
+    <div className="bg-black w-full h-10 hidden md:block">
       <Container>
         <div className="flex justify-between">
           <div className="flex">
             <div className="flex items-center m-1">
-              <MdMailOutline className="text-white text-lg m-1 " />
-              <p className="text-white text-lg ">info@example.com</p>
+              <MdMailOutline className="text-white text-lg m-1" />
+              <p className="text-white text-lg">info@example.com</p>
             </div>
-            <div className="flex items-center m-1 ">
+            <div className="flex items-center m-1">
               <FaLocationDot className="text-white text-lg m-1" />
-              <p className="text-white text-lg ">City, State, ZIP Code</p>
+              <p className="text-white text-lg">City, State, ZIP Code</p>
             </div>
           </div>
           <div className="flex items-center gap-5">
-            <select className="bg-transparent text-white border-none outline-none cursor-pointer p-1" name="" id="">
-              <option className="text-black bg-white " value="">English</option>
-              <option className="text-black bg-white" value="">Russian</option>
-              <option className="text-black bg-white" value="">Azerbaijan</option>
+            <select
+              className="bg-transparent text-white border-none outline-none cursor-pointer p-1"
+              name=""
+              id=""
+            >
+              <option className="text-black bg-white" value="">
+                English
+              </option>
+              <option className="text-black bg-white" value="">
+                Russian
+              </option>
+              <option className="text-black bg-white" value="">
+                Azerbaijan
+              </option>
             </select>
             <FaFacebookF className="text-white text-lg cursor-pointer" />
             <FaXTwitter className="text-white text-lg cursor-pointer" />
